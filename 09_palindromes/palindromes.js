216a003231e745aef193fc63211e121e4f9b505f
erika-lagos <144739255+erika-lagos@users.noreply.github.com>
2023-09-26 11:34:37 -0700

    Build palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const unusedChars = ['.', ',', '!',' '];
+    const chars = phrase.toLowerCase().split('').filter(char => !unusedChars.includes(char));
+    for (i = 0; i < chars.length; i++) {
+        if (chars[i] !== chars[chars.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
