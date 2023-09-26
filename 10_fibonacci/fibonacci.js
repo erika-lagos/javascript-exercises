27cf8a066fa859a8deed4a8c804eeff7f9e1d2d5
erika-lagos <144739255+erika-lagos@users.noreply.github.com>
2023-09-26 11:40:29 -0700

    Build fibonacci sequence finder

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = +number;
+    if (number < 1) return 'OOPS';
+    if (number === 1) return 1;
+    if (number === 2) return 1;
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
